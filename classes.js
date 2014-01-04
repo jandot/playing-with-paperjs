2da7179c050b8bd9d7d09d9c958c7a856aaba9c5
Jan Aerts <jan.aerts@gmail.com>
2014-01-04 20:01:24 +0100

    Sketch with custom classes

---
+++ classes.js
@@ -0,0 +1,19 @@
+function Person(x, y, color) {
+   this.x = x;
+   this.y = y;
+   this.color = color;
+}
+
+Person.prototype.draw = function() {
+    var path = new Path();
+    console.log(this.color);
+    path.strokeColor = this.color;
+    path.strokeWidth = 3;
+    path.add(new Point(this.x, this.y));
+    path.add(new Point(this.x+20, this.y+20));
+}
+
+var person1 = new Person(50,150,"red");
+var person2 = new Person(75,200,"green");
+person1.draw();
+person2.draw();
